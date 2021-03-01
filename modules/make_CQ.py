9e29b18fa877c90d4440cd768672b5231a36c7ae
Hanmin Qi <512240272@qq.com>
2021-03-01 22:34:26 +0800

    int转string修复

--- modules/make_CQ.py
+++ modules/make_CQ.py
@@ -2,11 +2,11 @@
 class make_cq:
     # 拼接回复CQ码
     def make_reply_cq(message_id):
-        return "[CQ:reply,id=" + message_id + "]"
+        return "[CQ:reply,id=" + str(message_id) + "]"
 
     # 拼接艾特CQ码
     def make_at_cq(qq):
-        return "[CQ:at,qq=" + qq + "]"
+        return "[CQ:at,qq=" + str(qq) + "]"
 
     # 拼接录音CQ码
     def make_record_cq(file, magic=0, timeout=-1, cache=1, proxy=1):
@@ -24,8 +24,8 @@ class make_cq:
 
     # 拼接表情CQ码
     def make_face_cq(id):
-        return "[CQ:face,id=" + id + "]"
+        return "[CQ:face,id=" + str(id) + "]"
 
     # 拼接群聊戳一戳CQ码
     def make_poke_cq(qq):
-        return "[CQ:poke,qq=" + qq + "]"
+        return "[CQ:poke,qq=" + str(qq) + "]"
